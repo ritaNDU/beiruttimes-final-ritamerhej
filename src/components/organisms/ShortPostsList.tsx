5b66dbae8906af0d2c87e7c3b8c24b7b42b7cc9d
rita <rita.merhej@hotmail.com>
2024-04-30 12:24:32 +0300

    feat/ add comments

--- src/components/organisms/ShortPostsList.tsx
+++ src/components/organisms/ShortPostsList.tsx
@@ -3,6 +3,10 @@ import Post from '../../data/post.type';
 import PostCard from '../molecules/PostCard';
 import {StyleProp, ViewStyle} from 'react-native';
 
+// I am using this component to display very short lists.
+// Like the topPostsToday list that is only at most 10 posts.
+// For bigger lists, I am using the PostList component.
+
 type Props = {
   posts: Post[];
   styleProp?: StyleProp<ViewStyle>;
